3d67269fed71ae4a915fe54eca8871e07bf3f680
DanielOlvera <josue_olver@yahoo.com.mx>
2016-10-20 13:31:27 -0500

    Commented HelloWorld

--- app/src/main/java/com/example/daniel/gradletask/MainActivity.java
+++ app/src/main/java/com/example/daniel/gradletask/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     // Example of a call to a native method
     //TextView tv = (TextView) findViewById(R.id.sample_text);
     //tv.setText(stringFromJNI());
+
+        //HelloWorld
     }
 
     /**
